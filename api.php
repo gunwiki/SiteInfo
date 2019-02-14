e8d690887d2ea3efda09ba03a689d3314395b2e6
RazeSoldier <razesoldier@outlook.com>
2019-02-14 18:02:31 +0800

    重写api.php并添加一个API模块

--- api.php
+++ api.php
@@ -9,19 +9,8 @@ require 'config.php';
 require __DIR__ . '/vendor/autoload.php';
 
 if (isset($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'viewcount':
-            getViewCount();
-            break;
-        default:
-            http_response_code(510);
-            echo json_encode([
-                'error' => [
-                    'action' => 'Unsupported action'
-                ]
-            ]);
-            die;
-    }
+    $data = routeRequest($_GET);
+    sendResponse($data);
 } else {
     http_response_code(510);
     echo json_encode([
@@ -32,11 +21,60 @@ if (isset($_GET['action'])) {
     die;
 }
 
-function getViewCount() {
+/**
+ * 路由请求
+ * @return array 返回获取到的数据
+ */
+function routeRequest(array $request) : array
+{
+    $data = [];
+    if (strpos($request['action'], '|') === false) {
+        $action = [$request['action']];
+    } else {
+        $action = explode('|', $request['action']);
+    }
+    foreach ($action as $item) {
+        switch ($item) {
+            case 'viewcount':
+                $data += getViewCount();
+                break;
+            case 'innodbinfo':
+                $data += getInnoDBBufferHitRate();
+                break;
+            default:
+                $data['error'][$item] = 'Unsupported action';
+        }
+    }
+    return $data;
+}
+
+/**
+ * 发送响应
+ * @param array $data
+ * @param int $httpStatusCode
+ */
+function sendResponse(array $data, int $httpStatusCode = 200) : void
+{
+    http_response_code($httpStatusCode);
+    echo json_encode($data);
+    die;
+}
+
+function getViewCount() : array
+{
     global $cfgAccessLogPath;
     $file = new SplFileObject($cfgAccessLogPath, 'r');
     $app = new GunWiki\SiteInfo\AnalysisSiteAccessLog($file);
     $res['ViewCount'] = $app->run();
-    echo json_encode($res);
+    return $res;
+}
+
+function getInnoDBBufferHitRate() : array
+{
+    global $cfgDBHost, $cfgDBUsername, $cfgDBPassword;
+    $app = new GunWiki\SiteInfo\InnoDBBufferHitRate($cfgDBHost, $cfgDBUsername, $cfgDBPassword);
+    return [
+        'InnoDBBufferHitRate' => $app->get(),
+    ];
 }
 
